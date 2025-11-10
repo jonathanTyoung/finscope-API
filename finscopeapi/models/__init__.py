16a4f081d062048ea9870e57897509dbf3fbad4d
Jon Young <youngt.jonathan>
2025-11-10 14:37:49 -0600

    django properly installed

--- finscopeapi/models/__init__.py
+++ finscopeapi/models/__init__.py
@@ -1,1 +1,6 @@
-from .user import *
+from .user import User
+from .report import Report
+from .portfolio import Portfolio
+from .esg_company import ESGCompany
+from .transaction import Transaction
+from .holding import Holding
